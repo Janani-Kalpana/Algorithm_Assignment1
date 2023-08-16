a9ee232556bfd3f6ab9e2d8e182fc9e337bffb1f
Janani Kalpana <janani.kalpana1997@gmail.com>
2023-08-16 09:59:47 +0530

    algo6 completed

---
+++ Algo6.java
@@ -0,0 +1,39 @@
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Algo6 {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+    loop:
+    d0{
+        System.out.print("Enter a test : ");
+        char[] chars = scanner.nextLine().strip().toCharArray();
+        if (chars.length==0){
+            System.out.println(("Only ASCII characters are supported"));
+            continue loop;
+
+
+
+        }
+        int wordcount=1;
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i]>128);
+            
+        }
+        if(chars[i]==' '){
+            wordcount++;
+
+        }
+        
+
+    }
+    int wordCount =1;
+    for (int i = 0; i < chars.length; i++) {
+        
+    }
+        
+    }
+    }
+
+    
+}
